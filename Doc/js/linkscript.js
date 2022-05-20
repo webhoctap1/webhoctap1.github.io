ec1901cb5859853e34521cf1b59b2c530b481add
Học lập trình FREE <98959228+webhoctap1@users.noreply.github.com>
2022-05-20 13:17:53 +0700

    Update linkscript.js

--- Doc/js/linkscript.js
+++ Doc/js/linkscript.js
@@ -1,8 +1,4 @@
 function Search() {
 
     var a = document.getElementById("search").value;
-    if (a === "Báo mới") {
-        window.open("https://baomoi.com");
-    }
-    return true;
-}
+    if (a === "home") {self.location = "index.html";}else if (a === "Home"){self.location = "index.html";} return true;}
