aa3291822348d78f13ce519b8dad28df880a3dd7
Học lập trình FREE <98959228+webhoctap1@users.noreply.github.com>
2022-06-05 12:02:27 +0700

    Update linkscript.js

--- Doc/js/linkscript.js
+++ Doc/js/linkscript.js
@@ -1,4 +1,12 @@
 function Search() {
 
     var a = document.getElementById("search").value;
-    if (a === "home") {self.location = "index.html";}else if (a === "Home"){self.location = "index.html";}  if (a === "Wikipedia") {self.location = "https://phungbatam.github.io/wikipedia-tim-kiem-khoa-hoc-thu-vien-lap-trinh/";} else if (a === "wikipedia") {self.location = "https://phungbatam.github.io/wikipedia-tim-kiem-khoa-hoc-thu-vien-lap-trinh/";}return true;}
+    if (a === "home") {
+        self.location = "index.html";
+        }else if (a === "Home"){
+              self.location = "index.html";
+        }  if (a === "Wikipedia") {
+               self.location = "https://phungbatam.github.io/wikipedia-tim-kiem-khoa-hoc-thu-vien-lap-trinh/";
+        } else if (a === "wikipedia") {
+               self.location = "https://phungbatam.github.io/wikipedia-tim-kiem-khoa-hoc-thu-vien-lap-trinh/";}
+}
